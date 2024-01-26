8192a7def0d7ee1f7e643200d0c57fe8475b37f9
Nathaniel C Napier <nattydevs@gmail.com>
2024-01-26 13:32:18 -0600

    Create multiplicationTable.js
    Your task, is to create N×N multiplication table, of size provided in parameter.
    
    For example, when given size is 3:
    
    1 2 3
    2 4 6
    3 6 9
    For the given example, the return value should be:
    
    [[1,2,3],[2,4,6],[3,6,9]]

---
+++ multiplicationTable.js
@@ -0,0 +1,13 @@
+multiplicationTable = function(size) {
+  // insert code here
+  let arr = [];
+  let fullArr = [];
+  for(i = 1; i <= size; i++){
+    arr.push(i);
+  };
+  for(j = 1; j <= size; j++){
+ function newFunct(num) {
+   return num * j;}
+ fullArr.push(arr.map(newFunct));
+}return fullArr;
+}
